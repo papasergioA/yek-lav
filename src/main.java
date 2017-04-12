0328510d9e2aa2504d09dabe0bbe7b48741d0a9f
Anthony P <papasergio.anthony@gmail.com>
2017-04-12 10:49:16 +0200

    base serveur/client

--- src/main.java
+++ src/main.java
@@ -3,12 +3,23 @@ public class main {
 
 	public static void main(String[] args) {
 
+		String host = "127.0.0.1";
+		int port = 2345;
+		
 		switch (args[0]) {
 		case "client":
 			System.out.println("Lancement du client");
+			Thread t = new Thread(new ClientConnexion(host, port));
+			t.start();
 			break;
 		case "serveur":
 			System.out.println("lancement du serveur");
+
+			TimeServer ts = new TimeServer(host, port);
+			ts.open();
+
+			System.out.println("Serveur initialisé.");
+
 			break;
 		default:
 			System.out.println("Erreur: Arguments attendu:\n client\nserveur nbServeurs");
